50612221a2a32ae0c1a01b96f5ebdf9ab0c7d557
sadekcric4 <sadekcric4@gmail.com>
2024-04-15 03:19:46 +0600

    error page added

--- src/Route/Router.jsx
+++ src/Route/Router.jsx
@@ -6,11 +6,13 @@ import Details from "../Pages/Details/Details";
 import Registration from "../Pages/Registration/Registration";
 import Login from "../Pages/Login/Login";
 import PrivateRoute from "./PrivateRoute";
+import Error from "../Pages/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
